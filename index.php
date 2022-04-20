ae1bd61f5840c65785db2e3214950dd2192c4e2f
Mangovich <104027161+Mangovich@users.noreply.github.com>
2022-04-20 11:40:04 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,72 +1,59 @@
 <?php
 
-session_start();
+/**
+ * @file
+ * Файл, который обслуживает все запросы страниц InstantCMS.
+ *
+ * Весь код InstantCMS выпущен в соответствии с лицензией GNU General Public License v2.
+ * Смотрите файлы license.en.txt и license.ru.txt в корне вашей установки копии InstantCMS.
+ * Сделано в InstantSoft, instantsoft.ru, instantcms.ru.
+ */
 
-define('DS', DIRECTORY_SEPARATOR);
-define('PATH', dirname(__FILE__) . DS);
-define('DOC_ROOT', str_replace(DS, '/', realpath($_SERVER['DOCUMENT_ROOT'])));
+/**
+ * Константа, по которой можно отследить текущий тип запуска CMS
+ */
+define('VALID_RUN', true);
 
-header("Content-type:text/html; charset=utf-8");
-mb_internal_encoding('UTF-8');
-date_default_timezone_set('UTC');
+/**
+ * Константа, наличие которой говорит о том, что нам нужны сессии
+ * и bootstrap.php их включит
+ */
+define('SESSION_START', true);
 
-include PATH . 'functions.php';
+header('Content-type:text/html; charset=utf-8');
+header('X-Powered-By: InstantCMS');
 
-$all_langs = get_langs();
-$default_lang = 'en';
+// Подключаем файл первоначальной инициализации окружения InstantCMS
+require_once 'bootstrap.php';
 
-if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-    $user_lang = strtolower(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
-    if (in_array($user_lang, $all_langs)) {
-        $default_lang = $user_lang;
-    }
+if ($config->emulate_lag) { usleep(350000); }
+
+//Запускаем роутинг
+$core->route($_SERVER['REQUEST_URI']);
+
+// Инициализируем шаблонизатор
+$template = cmsTemplate::getInstance();
+
+cmsEventsManager::hook('engine_start');
+
+// загружаем и устанавливаем страницы для текущего URI
+$core->loadMatchedPages();
+
+// Проверяем доступ
+if(cmsEventsManager::hook('page_is_allowed', true)){
+
+    //Запускаем контроллер
+    $core->runController();
+
 }
 
-if (isset($_REQUEST['lang'])) {
-    if (in_array($_REQUEST['lang'], $all_langs)) {
-        $_SESSION['install']['lang'] = $_REQUEST['lang'];
-        header('Location: ' . $_SERVER['SCRIPT_NAME']);
-        die;
-    }
-}
+// формируем виджеты
+$core->runWidgets();
 
-$is_lang_selected = isset($_SESSION['install']['lang']);
+//Выводим готовую страницу
+$template->renderPage();
 
-$lang = $is_lang_selected ? $_SESSION['install']['lang'] : $default_lang;
+cmsEventsManager::hook('engine_stop');
 
-define('LANG', $lang);
-
-include PATH . DS . 'languages' . DS . LANG . DS . "language.php";
-
-$steps = [
-    ['id' => 'start', 'title' => LANG_STEP_START],
-    ['id' => 'license', 'title' => LANG_STEP_LICENSE],
-    ['id' => 'php', 'title' => LANG_STEP_PHP_CHECK],
-    ['id' => 'paths', 'title' => LANG_STEP_PATHS],
-    ['id' => 'site', 'title' => LANG_STEP_SITE],
-    ['id' => 'database', 'title' => LANG_STEP_DATABASE],
-    ['id' => 'admin', 'title' => LANG_STEP_ADMIN],
-    ['id' => 'config', 'title' => LANG_STEP_CONFIG],
-    ['id' => 'cron', 'title' => LANG_STEP_CRON],
-    ['id' => 'finish', 'title' => LANG_STEP_FINISH]
-];
-
-$current_step = 0;
-
-if (is_ajax_request()) {
-    $step      = $steps[(int) (isset($_POST['step']) ? $_POST['step'] : 0)];
-    $is_submit = isset($_POST['submit']);
-    echo json_encode(run_step($step, $is_submit));
-    exit();
-}
-
-$step_result = run_step($steps[$current_step], false);
-
-echo render('main', [
-    'steps'            => $steps,
-    'is_lang_selected' => $is_lang_selected,
-    'lang'             => LANG,
-    'current_step'     => $current_step,
-    'step_html'        => $step_result['html'],
-    'langs'            => get_langs()
-]);
+// Останавливаем кеш
+cmsCache::getInstance()->stop();
